15d599306b25cc8c665073a798c5877b888b7aee
Mohamed Aissa Benfodda <magmmi@outlook.com>
2020-12-31 03:27:11 -0400

    first committt

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 exports.printmyname = function() {
     console.log("magmmi");//
+    console.log('\x1b[36m%s\x1b[0m', 'I am cyan');  //cyan
 }
+
+exports.shuffle = async array => {
+    for (var i = array.length - 1; i > 0; i--) {
+        var randomInt = Math.floor(Math.random() * (i + 1));
+        var a = array[i];
+        array[i] = array[randomInt];
+        array[randomInt] = a;
+    }
+    return array
+}
+
